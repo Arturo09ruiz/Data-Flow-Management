a930bf60b736921d1c0e1ab3726d31775858aa76
Gregory <gregory09ruiz@gmail.com>
2020-06-02 15:48:18 -0400

    Vistaready

--- vistas/modulos/Presidente-de-Estaca/miembros.php
+++ vistas/modulos/Presidente-de-Estaca/miembros.php
@@ -1,65 +1,65 @@
 
 <div class="content-wrapper">
 
-  <section class="content-header">
+<section class="content-header">
+  
+  <h1>
     
-    <h1>
-      
-      Visualizar Miembros
+    Visualizar Miembros
+  
+  </h1>
+
+  <ol class="breadcrumb">
     
-    </h1>
-
-    <ol class="breadcrumb">
-      
-      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
-      
-      <li class="active">Visualizar Miembros</li>
+    <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
     
-    </ol>
+    <li class="active">Visualizar Miembros</li>
+  
+  </ol>
 
-  </section>
+</section>
 
-  <section class="content">
+<section class="content">
 
-    <div class="box">
+  <div class="box">
 
 
-
-      <div class="box-body">
-        
-       <table class="table table-bordered table-striped dt-responsive tablaMiembrosPdtEstaca" width="100%">
+    <div class="box-body">
+      
+     <table class="table table-bordered table-striped dt-responsive tablaMiembrosPdtEstaca" width="100%">
+       
+      <thead>
+       
+       <tr>
          
-        <thead>
+         <th style="width:10px">#</th>
+         <th>NCM</th>
+         <th>Nombre Completo</th>
+         <th>Email</th>
+         <th>N° De Telf</th>
+
          
-         <tr>
-           
-           <th style="width:10px">#</th>
-           <th>NCM</th>
-           <th>Nombre Completo</th>
-           <th>Email</th>
-           <th>N° De Telf</th>
+       </tr> 
 
-           
-         </tr> 
+      </thead>      
 
-        </thead>      
+     </table>
 
-       </table>
-
-       <input type="hidden" value="<?php echo $_SESSION['perfil']; ?>" id="perfilOculto">
-       <input class="idestaca" type="hidden" value="<?php echo $_SESSION['idestaca']; ?>" id="idestaca">
-       <input type="hidden" value="<?php echo $_SESSION['idconsejo']; ?>" id="idconsejo">
-       <input type="hidden" value="<?php echo $_SESSION['idpais']; ?>" id="idpais">
+     <input type="hidden" value="<?php echo $_SESSION['perfil']; ?>" id="perfilOculto">
+     <input class="idbarrio" type="hidden" value="<?php echo $_SESSION['idbarrio']; ?>" id="idbarrio">
+     <input type="hidden" value="<?php echo $_SESSION['idestaca']; ?>" id="idestaca">
+     <input type="hidden" value="<?php echo $_SESSION['idconsejo']; ?>" id="idconsejo">
+     <input type="hidden" value="<?php echo $_SESSION['idpais']; ?>" id="idpais">
 
 
 
 
-
-      </div>
 
     </div>
 
-  </section>
+  </div>
+
+</section>
 
 </div>
 
