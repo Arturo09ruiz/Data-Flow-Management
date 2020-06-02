186b9c7f0c8c6857431d383cb8723779fa2b4b27
Gregory <gregory09ruiz@gmail.com>
2020-06-02 17:07:30 -0400

    Model-view-ready

---
+++ modelos/miembros-gerente.modelo.php
@@ -0,0 +1,46 @@
+
+<?php
+
+require_once "conexion.php";
+
+class ModeloMiembrosGerente{
+
+
+
+
+	static public function mdlMostrarMiembrosGerente($tabla, $item, $valor, $orden, $consejo){
+
+		if($item != null){
+
+			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE `idconsejo` = $consejo");
+
+			$stmt -> bindParam(":".$item, $valor, PDO::PARAM_STR);
+
+			$stmt -> execute();
+
+			return $stmt -> fetch();
+
+		}else{
+
+			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE `idconsejo` = $consejo");
+
+			$stmt -> execute();
+
+			return $stmt -> fetchAll();
+
+		}
+
+		$stmt -> close();
+
+		$stmt = null;
+
+	}
+
+
+
+	
+	
+	
+
+
+}
