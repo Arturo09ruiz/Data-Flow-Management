6a9cf1893e2a9f00f20e4a8beff106ea6a9c5d9e
Gregory <gregory09ruiz@gmail.com>
2020-06-04 20:29:55 -0400

    Added-Views

---
+++ vistas/modulos/Setenta-Consejo/menu.php
@@ -0,0 +1,117 @@
+<?php
+
+if ($_SESSION["perfil"] == "Gerente") {
+
+	echo '
+<aside class="main-sidebar">
+
+	 <section class="sidebar">
+
+		<ul class="sidebar-menu">
+
+
+
+			<li class="active">
+
+				<a href="inicio">
+
+					<i class="fa fa-home"></i>
+					<span>Inicio</span>
+
+				</a>
+
+			</li>
+
+
+
+			<li>
+
+			<a href="miembros">
+				
+				<i class="fa fa-circle-o"></i>
+				<span>Visualizar Miembros</span>
+
+			</a>
+
+		</li>
+
+
+	<li class="treeview">
+
+				<a href="#">
+
+					<i class="fa fa-list-ul"></i>
+					
+					<span>Solicitud Medicinas </span>
+					
+					<span class="pull-right-container">
+					
+						<i class="fa fa-angle-left pull-right"></i>
+
+					</span>
+
+				</a>
+
+				<ul class="treeview-menu">
+					
+				
+					<li>
+
+						<a href="realizar-solicitud">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Realizar Solicitud</span>
+
+						</a>
+
+					</li>
+
+
+					<li>
+
+						<a href="#">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Reporte de Solicitudes</span>
+
+						</a>
+
+					</li>
+
+					
+
+				
+
+			</ul>
+
+			</li>
+
+		
+
+
+		</ul>
+
+	 </section>
+
+</aside>';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
