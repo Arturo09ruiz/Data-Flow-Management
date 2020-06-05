fa625b066c14c8596269b6be5b39e8fed8f37d6d
Gregory <gregory09ruiz@gmail.com>
2020-06-04 20:42:41 -0400

    Datatable.ready

---
+++ ajax/datatable-miembros-setenta-pais.ajax.php
@@ -0,0 +1,97 @@
+<?php
+
+require_once "../controladores/miembros-setenta-pais.controlador.php";
+require_once "../modelos/miembros-setenta-pais.modelo.php";
+
+
+require_once "../controladores/barrios.controlador.php";
+require_once "../modelos/barrios.modelo.php";
+
+
+require_once "../controladores/estaca.controlador.php";
+require_once "../modelos/estaca.modelo.php";
+
+class TablaMiembrosSetentaPais{
+
+ 	/*=============================================
+ 	 MOSTRAR LA TABLA DE MIEMBROS
+  	=============================================*/ 
+
+	public function mostrarTablaMiembrosSetentaPais(){
+
+		$item = null;
+    	$valor = null;
+		$orden = "id";
+        $pais = $_GET["pais"];
+
+  		$miembrospais = ControladorMiembrosSetentaPais::ctrMostrarMiembrosSetentaPais($item, $valor, $orden, $pais);	
+
+  		if(count($miembrospais) == 0){
+
+  			echo '{"data": []}';
+
+		  	return;
+  		}
+		
+  		$datosJson = '{
+		  "data": [';
+
+		  for($i = 0; $i < count($miembrospais); $i++){
+
+		 
+		
+            $item = "id";
+            $orden = "id";
+            $valor = $miembrospais[$i]["idbarrio"];
+
+            $barrios = ControladorBarrios::ctrMostrarBarrios($item, $valor, $orden);
+         
+
+
+
+
+            $item = "id";
+            $orden = "id";
+            $valor = $miembrospais[$i]["idestaca"];
+
+            $estacas = ControladorEstaca::ctrMostrarEstaca($item, $valor, $orden);
+		 
+
+
+
+
+            
+
+		  	$datosJson .='[
+			      "'.($i+1).'",
+			      "'.$miembrospais[$i]["ncm"].'",
+			      "'.$miembrospais[$i]["nombre"].'",
+			      "'.$miembrospais[$i]["email"].'",
+                  "'.$miembrospais[$i]["telefono"].'",
+                  "'.$barrios["nombre"].'",
+                  "'.$estacas["nombre"].'"
+			    ],';
+
+		  }
+
+		  $datosJson = substr($datosJson, 0, -1);
+
+		 $datosJson .=   '] 
+
+		 }';
+		
+		echo $datosJson;
+
+
+	}
+
+
+
+}
+
+/*=============================================
+ACTIVAR TABLA DE MIEMBROS
+=============================================*/ 
+$activarMiembrosSetentaPais = new TablaMiembrosSetentaPais();
+$activarMiembrosSetentaPais -> mostrarTablaMiembrosSetentaPais();
+
