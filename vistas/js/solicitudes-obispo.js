c403f07976cd8aee9dca8d294041e37bb427e7fa
Gregory <gregory09ruiz@gmail.com>
2020-06-05 10:59:24 -0400

    Errror de Sintanxis

--- vistas/js/solicitudes-obispo.js
+++ vistas/js/solicitudes-obispo.js
@@ -1,10 +1,35 @@
 $(".nuevaNombre").change(function(){
 
-
     
-    
+	var idMiembro = $("#nuevaNombre").val();
+
+    $.ajax({
+	
+	
+			type: "POST",
+					url: "ajax/obtener-datos-miembro-obispo.ajax.php",
+					data:'idMiembro=' + idMiembro ,
+					dataType:"html",
+					asycn:false, //el error que cometí de sintaxis, es async
+						success: function (respuesta) {
+
+                            
+
+							// $("#g").val(respuesta);
+							// var formula = $("#nuevoMedida").val();
+							// var tipo = $("#g").val();
+
+							// final = tipo +"   "+ formula;
 
 
+							// $("#nuevaDescripcion").val(final);
+
+                            // // 
+
+					}
+	
+
+	})
 
 
 	
