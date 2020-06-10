5c4ccfa6e1cbdc0a53620ee340f4d0a8efc31719
Gregory <gregory09ruiz@gmail.com>
2020-06-10 12:39:02 -0400

    Js-added

--- vistas/js/solicitudes-obispo.js
+++ vistas/js/solicitudes-obispo.js
@@ -1,3 +1,100 @@
+
+/*=============================================
+CARGAR LA TABLA DINÁMICA DE PRODUCTOS
+=============================================*/
+
+$.ajax({
+
+	url: "ajax/datatable-solicitudes-obispo.ajax.php",
+	success:function(respuesta){
+		
+		console.log("respuesta", respuesta);
+
+	}
+
+})
+
+var barrio = $("#idbarrio").val();
+
+$('.tablaPedidos').DataTable( {
+    "ajax": "ajax/datatable-solicitudes-obispo.ajax.php?barrio="+barrio,
+    "deferRender": true,
+	"retrieve": true,
+	"processing": true,
+	 "language": {
+
+			"sProcessing":     "Procesando...",
+			"sLengthMenu":     "Mostrar _MENU_ registros",
+			"sZeroRecords":    "No se encontraron resultados",
+			"sEmptyTable":     "Ningún dato disponible en esta tabla",
+			"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_",
+			"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0",
+			"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+			"sInfoPostFix":    "",
+			"sSearch":         "Buscar:",
+			"sUrl":            "",
+			"sInfoThousands":  ",",
+			"sLoadingRecords": "Cargando...",
+			"oPaginate": {
+			"sFirst":    "Primero",
+			"sLast":     "Último",
+			"sNext":     "Siguiente",
+			"sPrevious": "Anterior"
+			},
+			"oAria": {
+				"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+				"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+			}
+
+	}
+
+} );
+
+
+/*=============================================
+ELIMINAR Solicitud
+=============================================*/
+
+$(".tablaPedidos tbody").on("click", "button.btnEliminarPedido", function(){
+
+	var idPedido = $(this).attr("idPedido");
+
+	swal({
+
+		title: '¿Está seguro de borrar La Solicitud?',
+		text: "¡Si no lo está puede cancelar la accíón!",
+		type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Si, borrar Solicitud!'
+        }).then(function(result) {
+        if (result.value) {
+
+        	window.location = "index.php?ruta=realizar-solicitud&idPedido="+idPedido;
+
+        }
+
+
+	})
+
+})
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(".nuevaNombre").change(function(){
 
     
@@ -114,3 +211,4 @@ $.ajax({
 
 
 })
+
