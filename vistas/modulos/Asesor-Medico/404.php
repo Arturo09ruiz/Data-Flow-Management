5e5f666fe2b97f072b6ebea2858c61b1edd2d2c3
Gregory <gregory09ruiz@gmail.com>
2020-06-16 08:30:19 -0400

    Views-Added

---
+++ vistas/modulos/Asesor-Medico/404.php
@@ -0,0 +1,55 @@
+<div class="content-wrapper">
+
+  <section class="content-header">
+    
+    <h1>
+      
+      Página no encontrada
+    
+    </h1>
+
+    <ol class="breadcrumb">
+      
+      <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      
+      <li class="active">Página no encontrada</li>
+    
+    </ol>
+
+  </section>
+
+  <section class="content">
+
+    <div class="error-page">
+      
+      <h2 class="headline text-primary">404</h2> 
+
+      <div class="error-content">
+
+        <h3>
+
+          <i class="fa fa-warning text-primary"></i> 
+
+          Ooops! Página no encontrada.
+
+        </h3>
+
+        <p>
+        
+           Ingresa al menú lateral y allí podrás encontrar las páginas disponibles. 
+        
+        </p>
+        
+        <p>
+        
+          Si Piensas que esto es un error comunicate con el Soporte Tecnico        
+        </p>
+
+
+      </div>
+
+    </div>  
+
+  </section>
+
+</div>
---
+++ vistas/modulos/Asesor-Medico/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2020 <a href="" target="_blank">Arturo Ruiz</a> & <a target="_blank" href="">Henry Silva</a></strong>
+
+
+
+</footer>
