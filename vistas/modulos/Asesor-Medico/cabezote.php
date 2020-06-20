0ee583f4f58cc5b1b55342dcc412297edba519b9
Gregory <gregory09ruiz@gmail.com>
2020-06-20 07:43:18 -0400

    Cabezote_Fixed

--- vistas/modulos/Asesor-Medico/cabezote.php
+++ vistas/modulos/Asesor-Medico/cabezote.php
@@ -7,7 +7,8 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>DFM</b></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Flow</b> Up</span>
+      <span class="logo-lg"><b>Data Flow</b> Management</span>
+	  
     </a>
 
 	<!--=====================================
