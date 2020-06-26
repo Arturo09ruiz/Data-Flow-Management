bbcb0fb0ce388b9905d300345e1792af7c71867f
Gregory <gregory09ruiz@gmail.com>
2020-06-25 20:53:04 -0400

    medicamento.modelo.import.in.the.autoload

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once "controladores/plantilla.controlador.php";
 require_once "controladores/usuarios.controlador.php";
 require_once "controladores/categorias.controlador.php";
@@ -9,70 +8,33 @@ require_once "controladores/ventas.controlador.php";
 
 
 
-//ADMINISTRADOR CONTROLADORES
+
+
+
 require_once "controladores/pais.controlador.php";
 require_once "controladores/consejo.controlador.php";
 require_once "controladores/estaca.controlador.php";
 require_once "controladores/barrios.controlador.php";
-
-
-
-
-
-//OBISPO CONTROLADORES
 require_once "controladores/miembros.controlador.php";
-
-
 require_once "controladores/solicitudes.controlador.php";
-
-
-
-
-
-//pdtestacacontroladores
 require_once "controladores/miembros-pdt-estaca.controlador.php";
-
-//GERENTECONTROLADORES
-
 require_once "controladores/miembros-gerente.controlador.php";
 require_once "controladores/entregados.controlador.php";
-
-
-
-
-
-//setentapais
 require_once "controladores/miembros-setenta-pais.controlador.php";
 require_once "controladores/desaprobar.controlador.php";
 require_once "controladores/entregar.controlador.php";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 require_once "modelos/usuarios.modelo.php";
 require_once "modelos/categorias.modelo.php";
 require_once "modelos/productos.modelo.php";
 require_once "modelos/clientes.modelo.php";
+require_once "modelos/medicamento.modelo.php";
+
 require_once "modelos/ventas.modelo.php";
 require_once "extensiones/vendor/autoload.php";
+
+
+
+
 
 
 
@@ -82,34 +44,16 @@ require_once "modelos/estaca.modelo.php";
 require_once "modelos/barrios.modelo.php";
 require_once "modelos/consejo.modelo.php";
 require_once "modelos/entregados.modelo.php";
-
-
-
-//Obispo Controladores
 require_once "modelos/miembros.modelo.php";
 require_once "modelos/solicitudes.modelo.php";
-
-
-
-//pdtestacamodelo
 require_once "modelos/miembros-pdt-estaca.modelo.php";
 require_once "modelos/entregar.modelo.php";
-
-
-
-
-
-//gerentemodelo
 require_once "modelos/miembros-gerente.modelo.php";
-
-
-
-
-
-//setentapais
 require_once "modelos/miembros-setenta-pais.modelo.php";
-
 require_once "modelos/desaprobar.modelo.php";
+
+
+
 
 
 
